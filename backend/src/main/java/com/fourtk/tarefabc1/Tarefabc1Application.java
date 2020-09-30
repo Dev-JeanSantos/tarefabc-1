9a2c0a46addf8e4c3f2b71626342df7892f43f8e
Jean Santos <jeancbsan@gmail.com>
2020-09-30 11:28:54 -0300

    PROJECT CREATED

---
+++ backend/src/main/java/com/fourtk/tarefabc1/Tarefabc1Application.java
@@ -0,0 +1,13 @@
+package com.fourtk.tarefabc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarefabc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarefabc1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fourtk/tarefabc1/Tarefabc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourtk.tarefabc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarefabc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
