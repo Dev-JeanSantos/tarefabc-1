0e1c0b9eb33fbc38f59c994758273274bb6936d0
Jean Santos <jeancbsan@gmail.com>
2020-09-30 17:06:51 -0300

    ClientDTO created

--- backend/src/main/java/com/fourtk/tarefabc1/services/ClientService.java
+++ backend/src/main/java/com/fourtk/tarefabc1/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.fourtk.tarefabc1.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fourtk.tarefabc1.dto.ClientDTO;
 import com.fourtk.tarefabc1.entities.Client;
 import com.fourtk.tarefabc1.repositories.ClientRepository;
 
@@ -16,12 +18,12 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
+	public List<ClientDTO> findAll(){
 		
-		return repository.findAll();
+		List<Client> list = repository.findAll();
 		
-		
-		
+		return list.stream().map(x -> new ClientDTO(x)).collect
+				(Collectors.toList());	
 	}
 
 }
